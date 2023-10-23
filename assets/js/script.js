c88c01e5ef25766f0d1260b23ff1f676372ef660
David Eriksson <david.eriksson@lundkroken.se>
2023-10-23 16:01:40 +0200

    feat: adds script for dice roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 // JavaScript code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "roll-dice") {
+                rollDice();
+            } else {
+                // TODO
+            }
+        });
+    }
+
+});
+
+function rollDice() {
+    let dice = Math.floor(Math.random() * 20) + 1;
+    displayresult(dice);
+    console.log(`Dice roll: ${dice}`);
+};
