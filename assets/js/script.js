e62e367ca52988008d5eb9f9c1cdb6bbe6bcb374
David Eriksson <david.eriksson@lundkroken.se>
2023-10-24 11:01:42 +0200

    feat: add basic game function for turn-based combat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,76 @@
-// JavaScript code goes here
-
+// Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "roll-dice") {
-                rollDice();
-            } else {
-                // TODO
-            }
-        });
-    }
-
+    gameLoop();
 });
+
+// Player stats
+const player = {
+    name: "Adventurer",
+    hitPoints: 100,
+    currentHitPoints: 100,
+    armorClass: 13,
+    attackPower: 10,
+};
+  
+// Monster stats
+const monsters = [
+    {
+        name: "Goblin",
+        hitPoints: 7,
+        currentHitPoints: 7,
+        armorClass: 15,
+        attackPower: 5,
+    },
+    {
+        name: "Orc",
+        hitPoints: 15,
+        currentHitPoints: 15,
+        armorClass: 13,
+        attackPower: 9,
+    },
+];
+
+// Flag to indicate whose turn it is
+let isPlayerTurn = true;
+
+// Player turn function
+function playerTurn() {
+    console.log("PLAYERS TURN")
+    let dice = rollDice();
+    console.log(`Dice roll: ${dice}`); // REMOVE THIS
+    displayresult(dice);
+    isPlayerTurn = false;
+    gameLoop();
+}
+
+// Monsters turn function
+function monsterTurn() {
+    console.log("MONSTERS TURN")
+    let dice = rollDice();
+    console.log(`Dice roll: ${dice}`); // REMOVE THIS
+    isPlayerTurn = true;
+    gameLoop();
+}
+
+// Main game function
+function gameLoop() {
+  if (isPlayerTurn) {
+    // It's the player's turn
+    // Wait for the player to take it's action
+    let playerActionBtn = document.getElementById('attack');
+    playerActionBtn.addEventListener('click', playerTurn);
+  } else {
+    // It's the monster's turn
+    monsterTurn(); // Trigger the monster's turn
+  }
+
+  // Here the game shoud update the UI
+  // and check for game over conditions
+}
 
 function rollDice() {
     let dice = Math.floor(Math.random() * 20) + 1;
-    displayresult(dice);
-    console.log(`Dice roll: ${dice}`); // REMOVE THIS
+    return dice;
 };
 
 function displayresult(dice) {
